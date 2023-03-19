4627eaa3c5fea2878d8b7fb53d76d7b32cc9843a
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-03-19 12:15:35 +0300

    update on single upload

--- singleupload.php
+++ singleupload.php
@@ -26,7 +26,7 @@ $i = 1;
 
 $folder_container = "assets/music/";
 
-addTrackContainer($artWorkFile, $tag, $conn,$id, $albumTitle, $artistId, $genreId, $releaseDate,$description);
+addTrackContainer($artWorkFile, $tag, $conn, $id, $albumTitle, $artistId, $genreId, $releaseDate, $description);
 
 
 //// set folders for upload
@@ -56,66 +56,86 @@ addTrackContainer($artWorkFile, $tag, $conn,$id, $albumTitle, $artistId, $genreI
 //}
 
 
-function addTrackContainer($album_artWorkFile, $albumtage, $conn, $id, $albumTitle, $albumArtist, $albumGenre,$release, $albumDescription)
+function addTrackContainer($album_artWorkFile, $albumtage, $conn, $id, $albumTitle, $albumArtist, $albumGenre, $release, $albumDescription)
 {
 
-
-    if ($albumtage == 'music') {
-        $folder_container = "assets/images/artwork/";
-        $target_file = $folder_container . $album_artWorkFile['name'];
-        $dbtarget_file = "https://artist.mwonya.com/assets/images/artwork/" . $album_artWorkFile['name'];
-    } elseif ($albumtage == 'podcast') {
-        $folder_container = "assets/images/podcastalbumartwork/";
-        $target_file = $folder_container . $album_artWorkFile['name'];
-        $dbtarget_file = "https://artist.mwonya.com/assets/images/podcastalbumartwork/" . $album_artWorkFile['name'];
-    } elseif ($albumtage == 'dj') {
-        $folder_container = "assets/images/podcastalbumartwork/";
-        $target_file = $folder_container . $album_artWorkFile['name'];
-        $dbtarget_file = "https://artist.mwonya.com/assets/images/podcastalbumartwork/" . $album_artWorkFile['name'];
-    } elseif ($albumtage == 'poem') {
-        $folder_container = "assets/images/poemsartwork/";
-        $target_file = $folder_container . $album_artWorkFile['name'];
-        $dbtarget_file = "https://artist.mwonya.com/assets/images/poemsartwork/" . $album_artWorkFile['name'];
-    } else {
-        echo "Media Tag is Not Provided";
+    // Check if album tag is provided
+    if (empty($albumtage)) {
+        echo "<span class='fileexistdanger' style='color: red;'>Media Tag is Not Provided</span>" . "\n";
         return;
     }
 
+    // Define folder container and target file path based on album tag
+    switch ($albumtage) {
+        case 'music':
+            $folder_container = "assets/images/artwork/";
+            break;
+        case 'podcast':
+        case 'dj':
+            $folder_container = "assets/images/podcastalbumartwork/";
+            break;
+        case 'poem':
+            $folder_container = "assets/images/poemsartwork/";
+            break;
+        default:
+            echo "<span class='fileexistdanger' style='color: red;'>Invalid Media Tag</span>" . "\n";
+            return;
+    }
+    $target_file = $folder_container . $album_artWorkFile['name'];
+    $dbtarget_file = "https://artist.mwonya.com/" . $target_file;
+
+    // Check if folder container exists, if not, create it
     if (!file_exists($folder_container)) {
         mkdir($folder_container, 0777, true);
-    } else {
-
-        $checkduplicate = $conn->prepare("SELECT * FROM albums WHERE id=:id");
-        $checkduplicate->execute(array(':id' => $id));
-
-        if ($checkduplicate->rowCount() >= 1) {
-            echo $albumTitle . "<span class='fileexistdanger' style='color: red;'>file already Exists in Database</span>" . "\n";
-        } else {
-            if (move_uploaded_file($album_artWorkFile['tmp_name'], $target_file)) {
-
-                $sql = "INSERT INTO albums (id, title, artist, genre, artworkPath, tag, releaseDate, description, AES_code) VALUES (:id, :title, :artist, :genre, :artworkPath, :tag, :releaseDate, :description, 'single')";
-
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(':id', $id);
-                $stmt->bindParam(':title', $albumTitle);
-                $stmt->bindParam(':artist', $albumArtist);
-                $stmt->bindParam(':genre', $albumGenre);
-                $stmt->bindParam(':artworkPath', $dbtarget_file);
-                $stmt->bindParam(':tag', $albumtage);
-                $stmt->bindParam(':releaseDate', $release);
-                $stmt->bindParam(':description', $albumDescription);
-
-                if ($stmt->execute()) {
-                    echo $albumTitle . "<span class='checkeddone' style='color: green; font-weight:bold;'> Done</span>" . "\n";
-                } else {
-                    echo "Media upload to Database Failed" . "\n";
-                }
-            } else {
-                echo "Media Upload Failed. Try again" . "\n";
-            }
-        }
-
     }
+
+
+    // Check if album title, artist, genre, release date, and description are provided
+    if (empty($albumTitle) || empty($albumArtist) || empty($albumGenre) || empty($release) || empty($albumDescription)) {
+        echo "<span class='fileexistdanger' style='color: red;'>Please fill in all required fields</span>" . "\n";
+        return;
+    }
+
+    // Check if album artwork file is provided and is a valid image file
+    if (empty($album_artWorkFile) || !in_array(strtolower(pathinfo($album_artWorkFile['name'], PATHINFO_EXTENSION)), array('jpg', 'jpeg', 'png'))) {
+        echo "<span class='fileexistdanger' style='color: red;'>Invalid Album Artwork File. Please upload a valid image file (JPG, JPEG, PNG)</span>" . "\n";
+        return;
+    }
+
+    // Check if album with provided id already exists
+    $checkduplicate = $conn->prepare("SELECT * FROM albums WHERE id=:id");
+    $checkduplicate->execute(array(':id' => $id));
+    if ($checkduplicate->rowCount() >= 1) {
+        echo $albumTitle . "<span class='fileexistdanger' style='color: red;'>file already Exists in Database</span>" . "\n";
+        return;
+    }
+
+
+// Move album artwork file to target folder
+    if (move_uploaded_file($album_artWorkFile['tmp_name'], $target_file)) {
+
+        // Insert album information into database
+        $sql = "INSERT INTO albums (id, title, artist, genre, artworkPath, tag, releaseDate, description, AES_code) VALUES (:id, :title, :artist, :genre, :artworkPath, :tag, :releaseDate, :description, 'single')";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':title', $albumTitle);
+        $stmt->bindParam(':artist', $albumArtist);
+        $stmt->bindParam(':genre', $albumGenre);
+        $stmt->bindParam(':artworkPath', $dbtarget_file);
+        $stmt->bindParam(':tag', $albumtage);
+        $stmt->bindParam(':releaseDate', $release);
+        $stmt->bindParam(':description', $albumDescription);
+
+        if ($stmt->execute()) {
+            echo $albumTitle . "<span class='checkeddone' style='color: green; font-weight:bold;'> Done</span>" . "\n";
+        } else {
+            echo "Media upload to Database Failed" . "\n";
+        }
+    } else {
+        echo "Media Upload Failed. Try again" . "\n";
+    }
+
+
 }
 
 
