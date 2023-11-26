19823e73a6bc1d62ac6f3f40d19c094072176f6b
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-11-26 17:47:59 +0300

    update on album,song and artist availability

--- maven.js
+++ maven.js
@@ -1,1 +1,25 @@
 console.log("dero waa")
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    uploadFiles();
+}
+
+function uploadFiles() {
+    const url = 'https://upload.mwonya.com/process-upload';
+    const formData = new FormData(form);
+
+    const fetchOptions = {
+        method: 'post',
+        body: formData
+    };
+
+    fetch(url, fetchOptions);
+}
